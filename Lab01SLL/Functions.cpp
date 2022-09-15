674229477d401d5aed49367de2ba52818a6ce75f
doorjay <dorjekmp@gmail.com>
2022-09-15 14:21:57 -0700

    Last function and comments

--- Lab01SLL/Functions.cpp
+++ Lab01SLL/Functions.cpp
@@ -3,7 +3,8 @@
  * Hopefully this will not be needed at all!
  */
 
-// Problem 1, write a function that returns the last element in a linked list
+// Problem 1
+// Write a function that returns the last element in a linked list
 int AnyList::getLastElem() const
 {
    Node* current = ptrToFirst;
@@ -16,7 +17,8 @@ int AnyList::getLastElem() const
    return current->getData();
 }
 
-// Problem 2, wirte a function that returns true if all the elements in a list are odd
+// Problem 2
+// Write a function that returns true if all the elements in a list are odd
 bool AnyList::allOdds() const
 {
    Node* current = ptrToFirst;
@@ -34,7 +36,8 @@ bool AnyList::allOdds() const
    return true;
 }
 
-// Problem 3, write a function that deletes the first node in a linked list
+// Problem 3
+// Write a function that deletes the first node in a linked list
 void AnyList::deleteFirstNode()
 {
    if (ptrToFirst == nullptr)
@@ -49,8 +52,8 @@ void AnyList::deleteFirstNode()
    }
 }
 
-// Problem 4, write a function that compares two lists and
-// returns true if they have the same elemts in the same order
+// Problem 4
+// Write a function that compares two lists andreturns true if they have the same elements in the same order
 bool AnyList::compareLists(const AnyList& compare) const
 {
    // return false if the lists don't have the same number of elements
