e65f4991f1a012d76f1bb1fa309d9d7b013d5324
doorjay <dorjekmp@gmail.com>
2022-09-21 17:40:59 -0700

    insert method and printing forawrds/backwards

--- DoublyLinkedLists/DoublyList.cpp
+++ DoublyLinkedLists/DoublyList.cpp
@@ -1,2 +1,48 @@
 #include "DoublyList.h"
+#include <iostream>
 
+using namespace std;
+
+void DoublyList::insertFront(int newData)
+{
+    // if the list is empty, create a new node and store newData in data
+    if (first == nullptr)
+    {
+        first = new Node(newData, nullptr, nullptr);
+        last = first;
+    }
+    else
+    {
+        first = new Node(newData, nullptr, first);
+
+        // remember to connect prev to the olf "first" node
+        first->getNext()->setPrev(first);
+    }
+
+    // increase the count
+    ++count;
+}
+
+void DoublyList::printForward() const
+{
+    // start at first node and move until the end
+    Node* current = first;
+
+    while ( current != nullptr)
+    {
+        cout << current->getData();
+        current = current->getPrev();
+    }
+}
+
+void DoublyList::printReverse() const 
+{
+    // start at THE END and move until begining
+    Node* current = last;
+
+    while (current != nullptr)
+    {
+        cout << current->getData();
+        current = current->getPrev();
+    }
+}
