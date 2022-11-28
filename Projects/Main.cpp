b7536746a05991925a73fd02b3bdbe59b461691e
doorjay <dorjekmp@gmail.com>
2022-11-27 16:27:43 -0800

    AHHH

--- Projects/Main.cpp
+++ Projects/Main.cpp
@@ -14,19 +14,6 @@ using namespace std;
 
 int main()
 {
-    Course java;
-
-    java.setCourseNumber(150);
-    java.setCourseName("Java");
-    java.setCourseUnits(4);
-
-    int prereqs[] = {100, 110};
-    java.setPrereqs(prereqs, 2);
-
-    cout << "Course number: " << java.getCourseNumber() << "\n";
-    cout << "Course name: " << java.getCourseName() << "\n";
-    cout << "Course units: " << java.getCourseUnits() << "\n";
-    // cout << "Course prerequisets: " << "\n";
 
     return 0;
 }
